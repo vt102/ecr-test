f09c7580ed5bb2c4d18972dcfa409a8d2586e804
Andy Cowell <andy@cowell.org>
2017-05-24 02:08:30 +0000

    change versioning to build number

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 eval $(aws ecr get-login)
 docker tag wichita-ecr-andy-docker-app:latest 052763798005.dkr.ecr.us-east-1.amazonaws.com/wichita-ecr-andy-docker-app:latest
 docker push 052763798005.dkr.ecr.us-east-1.amazonaws.com/wichita-ecr-andy-docker-app:latest
-docker tag wichita-ecr-andy-docker-app:latest 052763798005.dkr.ecr.us-east-1.amazonaws.com/wichita-ecr-andy-docker-app:${CIRCLE_SHA1}
-docker push 052763798005.dkr.ecr.us-east-1.amazonaws.com/wichita-ecr-andy-docker-app:${CIRCLE_SHA1}
+docker tag wichita-ecr-andy-docker-app:latest 052763798005.dkr.ecr.us-east-1.amazonaws.com/wichita-ecr-andy-docker-app:v${CIRCLE_BUILD_NUM}
+docker push 052763798005.dkr.ecr.us-east-1.amazonaws.com/wichita-ecr-andy-docker-app:v${CIRCLE_BUILD_NUM}
