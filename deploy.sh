ba565da218c5a571eabdae6a71d3a40727b0cd5c
Andy Cowell <andy@cowell.org>
2017-05-26 20:15:41 +0000

    added foo and bar

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,8 @@ docker tag wichita-ecr-andy-docker-app:latest 052763798005.dkr.ecr.us-east-1.ama
 docker push 052763798005.dkr.ecr.us-east-1.amazonaws.com/wichita-ecr-andy-docker-app:v${CIRCLE_BUILD_NUM}
 
 ./deployer.py v${CIRCLE_BUILD_NUM}
+
+for i in foo bar ; do
+    docker tag wichita-ecr-andy-docker-app:latest 052763798005.dkr.ecr.us-east-1.amazonaws.com/${i}:0.0.${CIRCLE_BUILD_NUM}
+    docker push 052763798005.dkr.ecr.us-east-1.amazonaws.com/${i}:0.0.${CIRCLE_BUILD_NUM}
+done
