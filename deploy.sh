77dfdd247745ff946f9c832ca0a72dd703bd1706
Andy Cowell <andy@cowell.org>
2017-05-27 05:28:36 +0000

    removed foo and bar

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ docker push 052763798005.dkr.ecr.us-east-1.amazonaws.com/wichita-ecr-andy-docker
 
 # ./deployer.py v${CIRCLE_BUILD_NUM}
 
-for i in foo bar ; do
-    docker tag wichita-ecr-andy-docker-app:latest 052763798005.dkr.ecr.us-east-1.amazonaws.com/${i}:0.0.${CIRCLE_BUILD_NUM}
-    docker push 052763798005.dkr.ecr.us-east-1.amazonaws.com/${i}:0.0.${CIRCLE_BUILD_NUM}
-done
+# for i in foo bar ; do
+#     docker tag wichita-ecr-andy-docker-app:latest 052763798005.dkr.ecr.us-east-1.amazonaws.com/${i}:0.0.${CIRCLE_BUILD_NUM}
+#     docker push 052763798005.dkr.ecr.us-east-1.amazonaws.com/${i}:0.0.${CIRCLE_BUILD_NUM}
+# done
 
 aws lambda invoke --function-name wichita-lambda-deployer-DeployerLambda-11BFLB8BHAYV6 --payload '{ "Version" : "v'${CIRCLE_BUILD_NUM}'" }' lambda-invoke-output.txt
